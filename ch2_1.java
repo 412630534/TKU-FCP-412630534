3bcc3b530c76d9e3671a66a861008869276c55f0
412630534 <412630534@o365.tku.edu.tw>
2024-07-02 12:19:15 +0800

    我的第一個JAVA程式

---
+++ ch2_1.java
@@ -0,0 +1,7 @@
+public class ch2_1 {
+    
+    public static void main(String arg[]){
+        System.out.println("Hello java!");
+        System.out.println("0702");
+    }
+}
---
+++ simple.java
@@ -0,0 +1,16 @@
+public class simple{//定義public類別ch2_1
+    public static void main(String[] args){//main()函數，主程式開始
+       int num1=2;//宣告整數num
+
+       int num2=3;
+        
+       float num3=4;
+       int num4=5;
+       System.out.println(num1+"cat are running");//印出自串及變數內容
+       
+       System.out.println(num2+"dogs are running");
+       System.out.println(num2+"birds are running");
+       System.out.println(num2+"tigers are running");
+
+
+}
